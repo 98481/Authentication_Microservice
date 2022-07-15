daf26c9f9d64ee3739c712aa7e3cc7529fe565a7
98481 <sreeharshitha2000@gmail.com>
2022-07-15 22:55:11 +0530

    Auth

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,7 @@
 insert into user (userid,upassword,uname) values ('admin','admin','admin');
 
-insert into user (userid,upassword,uname) values ('Harshitha','Harshitha','Harshitha');
-insert into user (userid,upassword,uname) values ('Nivas','Nivas','Nivas');
-insert into user (userid,upassword,uname) values ('Shivani','Shivani','Shivani');
-insert into user (userid,upassword,uname) values ('user1','user1','user1');
-insert into user (userid,upassword,uname) values ('user2','user2','user2');
-insert into user (userid,upassword,uname) values ('user3','user3','user3');
-insert into user (userid,upassword,uname) values ('user4','user4','user4');
+insert into user (userid,upassword,uname) values ('Harshitha@gmail.com','Harshitha','Harshitha@gmail.com');
+insert into user (userid,upassword,uname) values ('Nivas@gmail.com','Nivas','Nivas@gmail.com');
+insert into user (userid,upassword,uname) values ('Shivani@gmail.com','Shivani','Shivani@gmail.com');
+insert into user (userid,upassword,uname) values ('user1@gmail.com','user1','user1@gmail.com');
+insert into user (userid,upassword,uname) values ('user2@gmail.com','user2','user2@gmail.com');
